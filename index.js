1ba25a3e3844ecb383f716f76698e71d152168ff
Ewa Szalwinska <e.szalwinska@gmail.com>
2025-10-04 12:46:11 -0700

    prompts user for input-faherheit degress and takes it checks if it is a number and returns C degress with a comment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function convertToCelcius(tempFahr) {
-  return Math.floor(((tempFahr - 32) * 5) / 9);
+  if (tempFahr === null || tempFahr.trim() === "") {
+    alert("Please enter a number!");
+    return;
+  }
+
+  let num = Number(tempFahr);
+  if (isNaN(num)) {
+    alert("Number! Enter a valid number!");
+    return;
+  } else {
+    return Math.floor(((tempFahr - 32) * 5) / 9);
+  }
 }
 
 function describeTemperature(tempFahr) {
   let tempCels = convertToCelcius(tempFahr);
-  let description;
 
+  if (tempCels === undefined) {
+    return "I said: Number! Enter a valid number";
+  }
+  let description;
   if (tempCels < 0) {
     description =
       "very cold. It means you need to start figthing with a polar bear for food";
@@ -16,7 +30,7 @@ function describeTemperature(tempFahr) {
   } else if (tempCels < 40) {
     description = "hot. You will boil with dolphins";
   } else {
-    description = "very hot. It's too hot to think.";
+    description = "very hot. It's too hot to think!";
   }
 
   return `${tempFahr} degrees Fahrenheit is ${tempCels} degrees Celsius, which is ${description}!`;
